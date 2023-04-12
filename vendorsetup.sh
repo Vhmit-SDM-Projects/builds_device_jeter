624b70a3d3c08b59762cf433f41a9dd5584dd5fd
Vhmit <praxedes.9630@gmail.com>
2023-04-12 07:11:18 -0300

    jeter: Remove script and HALs
    * Aosp has different hals than lineage
      so we won't need these.
    
    Signed-off-by: Vhmit <praxedes.9630@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
-echo -e "\033[33mDeseja clonar as dependências? (s/n)\n\033[0m"
-read resposta
 
-if [ "$resposta" = "s" ]; then
-echo -e "\033[32mInicializando...\033[0m"
 # Vendor
-echo ""
-git clone -b thirteen git@github.com:Team-NoobMaster69/vendor_motorola_jeter.git vendor/motorola/jeter
+echo "cloning vendor"
+git clone -b thirteen https://github.com/Team-NoobMaster69/vendor_motorola_jeter vendor/motorola/jeter
 echo ""
 
 # Kernel
-echo ""
-git clone -b aljeter-337 git@github.com:Team-NoobMaster69/kernel_motorola_msm8937.git kernel/motorola/msm8937
+echo "cloning kernel"
+git clone -b thirteen https://github.com/Vhmit/kernel_motorola_jeter kernel/motorola/msm8937
 echo ""
 
 # Dependencies
-echo ""
+echo "cloning dependencies"
 git clone -b lineage-18.1 https://github.com/LineageOS/android_external_sony_boringssl-compat external/sony/boringssl-compat
 git clone -b lineage-20 https://github.com/LineageOS/android_system_qcom system/qcom
 git clone -b lineage-20 https://github.com/LineageOS/android_hardware_motorola hardware/motorola
 echo ""
 
 # DtbTools Lineage
-echo ""
+echo "cloning dtbtools"
 mkdir out/
 mkdir out/host/
 mkdir out/host/linux-x86/
@@ -32,25 +28,3 @@ wget -c https://github.com/krasCGQ/scripts/raw/master/prebuilts/bin/dtbToolLinea
 chmod +777 dtbToolLineage
 cd ../../../..
 echo ""
-
-# HAL's
-cd hardware/qcom-caf/msm8996/audio
-git status | tee gitdata.txt
-if
-grep -q lineage-20.0-caf-msm8996 "gitdata.txt" ; then
-echo -e "\033[32mTudo no seu devido lugar, Boa compilação!!\n\033[0m"
-rm -rf gitdata.txt
-cd ../../../..
-else
-echo -e "\033[33mClonando as HALs...\033[0m"
-rm -rf hardware/qcom-caf/msm8996/audio
-git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-20.0-caf-msm8996 hardware/qcom-caf/msm8996/audio
-rm -rf hardware/qcom-caf/msm8996/media
-git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-20.0-caf-msm8996 hardware/qcom-caf/msm8996/media
-rm -rf hardware/qcom-caf/msm8996/display
-git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-20.0-caf-msm8996 hardware/qcom-caf/msm8996/display
-echo -e "\033[32m Tudo no seu devido lugar, Boa compilação!!\n\033[0m"
-fi
-else
-echo -e "\033[31mAs dependências foram puladas!!\n\033[0m"
-fi
