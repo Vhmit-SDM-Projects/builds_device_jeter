3f1af81d918c1919a10b0e2f4ed65591daa3b5d0
Vhmit <praxedes.9630@gmail.com>
2023-04-14 03:08:46 +0200

    jeter: Set USE_LEGACY_BOOTANIMATION
    * To avoid bootloop
    
    Signed-off-by: Vhmit <praxedes.9630@gmail.com>

--- derp_jeter.mk
+++ derp_jeter.mk
@@ -13,6 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+USE_LEGACY_BOOTANIMATION := true
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
